78be019f75bf96eef7fcd183e41215afb56c82da
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-20 02:07:27 -0400

    feat: Definida função para atualizar dados de usuarios

--- controller/user.js
+++ controller/user.js
@@ -23,6 +23,23 @@ export const addNewUser = (req, res) => {
     db.query(q, [values], (err) => {
         if(err) return res.json(err);
 
-        return res.status(200).json("User inserted on Database!");
+        return res.status(200).json("User inserted in Database!");
+    })
+}
+
+export const updateUser = (req, res) => {
+    const q = "UPDATE user SET `nome`=?, `email`=?, `fone`=?, `date_of_birth`=? WHERE `id`= ? ";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.date_of_birth,
+    ];
+
+    db.query(q, [...values, req.params.id], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("User updated successfully!");
     })
 }
