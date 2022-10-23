e8d6c7ad6c8df71cf297d58b310510b831beecda
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-23 12:17:04 -0400

    docs: Reajuste de padrão de segurança

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "bb90327f9c67eb",
-    password: "277b074c",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: "heroku_4073c21c6a2867f"
 })
