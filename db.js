2b3c1e1fc75516067bff3ef0584777bd77d51721
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-22 21:59:58 -0400

    Docs: Ajuste de docs para segurança da aplicação

--- db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql";
-
-export const db = mysql.createConnection({
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "bb90327f9c67eb",
-    password: "277b074c",
-    database: "heroku_4073c21c6a2867f",
-})
