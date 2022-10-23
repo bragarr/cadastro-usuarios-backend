fee46ed13d035bf4f925e6b9ae97271e7bdd4708
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-23 13:21:33 -0400

    docs: Reajuste de padrão de segurança

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import userRoutes from "./routes/users.js"
 
 import cors from "cors";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 3000;
-
-console.log(port);
 
 app.use("/", userRoutes)
 
