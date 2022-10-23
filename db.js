cff84d0b7ad5a282e63f001a06bf1b1e71fe6fdd
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-22 23:17:06 -0400

    docs: Ajustes para segurança da aplicação

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+require("dotenv").config();
+
+const hots = process.env.DB_HOST
 
 export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "bb90327f9c67eb",
+    password: "277b074c",
+    database: "heroku_4073c21c6a2867f"
 })
